93843f740891e5b0c7b92f2dad719b7b99a0ce60
Csaba Valyi <csaba_valyi@epam.com>
2014-03-07 17:04:26 +0100

    removing the magic number

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,16 @@
 'use strict';
+/*global d3*/
 
 angular.module('d3ExperimentApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
-    
+          
+    console.dir(d3);
     $scope.data = [4, 8, 15, 16, 23, 42, 55, 1];
+    var scale = d3.scale.linear()
+            .domain([0, d3.max($scope.data)])
+            .range([0,420]);
+    
     $scope.setWidth = function(number){
-        return {width: number*10+'px'};
-    };
+        return {width: scale(number)+'px'};
+      };
   }]);
