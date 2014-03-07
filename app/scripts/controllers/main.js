1de7ec3deb6c175db1574fd2283916ffbf4ae920
Csaba Valyi <csaba_valyi@epam.com>
2014-03-07 16:38:46 +0100

    bar chart 101

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('d3ExperimentApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', ['$scope', function ($scope) {
+    
+    $scope.data = [4, 8, 15, 16, 23, 42, 55, 1];
+    $scope.setWidth = function(number){
+        return {width: number*10+'px'};
+    };
+  }]);
